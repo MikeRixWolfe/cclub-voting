9850ce416b5881b20b5208a5ec10c00f37fd37db
MikeRixWolfe <michaelrixwolfe@gmail.com>
2017-03-06 18:11:29 -0500

    Move flashed errors to base, add logout endpoint, secure scripts helper, add results queries

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from app.views import auth
 app.register_blueprint(auth)
